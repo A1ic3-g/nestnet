c08223a25580c859ba7dd9730231853231f0abbd
Dom Binks <dominic.binks@gmail.com>
2024-11-02 14:59:01 +0000

    Add handler outlines

--- internal/service/service.go
+++ internal/service/service.go
@@ -7,11 +7,29 @@ import (
 
 const ADDR = "0.0.0.0:8080"
 
+// Test handler
 func testHandler(w http.ResponseWriter, r *http.Request) {
 	_, err := w.Write([]byte("Hello, world!\n"))
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+// Hello handler
+//
+// Send: hello message
+//
+// Receive: signed message
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+// Posts handler
+//
+// Send: number of most recent posts to get (optional)
+//
+// Receive: posts with signature
+func postsHandler(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func Start() {
