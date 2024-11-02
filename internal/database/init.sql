68ef3310a955a52713ce76d9a03be1c40d12b9c1
Dom Binks <dominic.binks@gmail.com>
2024-11-02 23:21:40 +0000

    Add all database methods

--- internal/database/init.sql
+++ internal/database/init.sql
@@ -1,16 +1,16 @@
 CREATE TABLE LocalUser (
     id INT PRIMARY KEY,
-    name VARCHAR(255),
-    pubX CHAR(32),
-    pubY CHAR(32),
-    privD CHAR(32),
-    address VARCHAR(255)
+    name VARCHAR(255) NOT NULL,
+    pubX CHAR(32) NOT NULL,
+    pubY CHAR(32) NOT NULL,
+    privD CHAR(32) NOT NULL,
+    address VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Posts(
     id INT PRIMARY KEY,
-    title VARCHAR(255),
-    body VARCHAR(1024),
+    title VARCHAR(255) NOT NULL,
+    body VARCHAR(1024) NOT NULL,
     imgmd5 VARCHAR(16),
     imgname VARCHAR(255)
 );
--- internal/database/queries/localuser.sql
+++ internal/database/queries/localuser.sql
@@ -1,2 +1,11 @@
 -- name: getName :one
 SELECT name FROM LocalUser;
+
+-- name: setName :exec
+UPDATE LocalUser SET name=$1;
+
+-- name: getPubKey :one
+SELECT (pubX, pubY) FROM LocalUser;
+
+-- name: getPrivKey :one
+SELECT privD FROM LocalUser;
