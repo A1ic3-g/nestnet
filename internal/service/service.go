0a821bd1dfc0e3b34f7dfebc8cd9bf5305c3d87a
Dom Binks <dominic.binks@gmail.com>
2024-11-02 18:05:10 +0000

    Add helloHandler
    Hello handler has been added that sends a hello request to another
    instance.

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,26 +1,73 @@
 package service
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/sha256"
+	"encoding/json"
 	"log"
+	"math/big"
 	"net/http"
+	"strconv"
+	"strings"
 )
+
+type helloReq struct {
+	addr string
+	x    *big.Int // Public key x
+	y    *big.Int // Public key y
+}
+
+type helloRes struct {
+	r *big.Int // Signature r
+	s *big.Int // Signature s
+}
 
 const ADDR = "0.0.0.0:8080"
 
 // Test handler
 func testHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("Hello, world!\n"))
-	if err != nil {
+	msg := "Hello, world!\n"
+	sent, err := w.Write([]byte(msg))
+	if sent != len([]byte(msg)) || err != nil {
 		log.Fatal(err)
 	}
 }
 
-// Hello handler
-//
-// Send: hello message
-//
-// Receive: signed message
-func helloHandler(w http.ResponseWriter, r *http.Request) {
+// helloHandler asks the given address to sign a hello message and verifies it using the given public key
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	var reqBody helloReq
+	msg := "HELLO"
+
+	err := json.NewDecoder(req.Body).Decode(&reqBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out, err := http.NewRequest(http.MethodPost, reqBody.addr, strings.NewReader(msg))
+	res, err := http.DefaultClient.Do(out)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var resBody helloRes
+	err = json.NewDecoder(res.Body).Decode(&resBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hash := sha256.Sum256([]byte(msg))
+	success := ecdsa.Verify(&ecdsa.PublicKey{
+		Curve: elliptic.P256(),
+		X:     reqBody.x,
+		Y:     reqBody.y,
+	}, hash[:], resBody.r, resBody.s)
+	successStr := strconv.FormatBool(success)
+
+	sent, err := w.Write([]byte(successStr))
+	if sent != len([]byte(successStr)) || err != nil {
+		log.Fatal(err)
+	}
 }
 
 // Posts handler
@@ -35,6 +82,7 @@ func postsHandler(w http.ResponseWriter, r *http.Request) {
 func Start() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", testHandler)
+	mux.HandleFunc("/hello", helloHandler)
 
 	err := http.ListenAndServe(ADDR, mux)
 	if err != nil {
